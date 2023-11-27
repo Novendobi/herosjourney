19224473db416d7cc023efebb2d65b098ecd98f9
Novend <nchidinma4@gmail.com>
2023-11-27 08:01:07 +0100

    Update settings.py

--- smartlearning/settings.py
+++ smartlearning/settings.py
@@ -43,6 +43,7 @@ CSRF_TRUSTED_ORIGINS = [
     # 'http://www.herosjourney.one',
     'https://www.herosjourney.one',
     'https://herosjourney.one',
+    'https://web-production-c2c05.up.railway.app',
 ]
 
 SITE_ID = 1
